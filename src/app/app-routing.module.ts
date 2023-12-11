ba5277b5e2c372c083c49412dd066975742c06b8
kubya1103 <kubyasabavat@gmail.com>
2023-12-11 11:25:04 +0530

    colore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
-import { CreateemployeComponent } from './createemploye/createemploye.component';
+import { CreateemployeComponent  } from './createemploye/createemploye.component';
 import { AllemployesComponent } from './allemployes/allemployes.component';
+import { NotifyGuard } from './notify.guard';
 
 const routes: Routes = [
   {path:'',component:LoginComponent},
   {path:'dashboard',component:DashboardComponent, children:[
     {path:'home',component:HomeComponent},
-    {path:'createemploye',component:CreateemployeComponent},
-    {path:'allemployes',component:AllemployesComponent}
+    {path:'createemploye',canActivate:[NotifyGuard],component:CreateemployeComponent},
+    {path:'allemployes',canActivate:[NotifyGuard],component:AllemployesComponent}
   ]},
 ];
 
